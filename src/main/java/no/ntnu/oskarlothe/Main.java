70de040a92a138a317fa6685042f3600223834d0
Oskar <oskarlo@stud.ntnu.no>
2023-06-07 23:49:30 +0200

    Updated pom.xml

---
+++ src/main/java/no/ntnu/oskarlothe/Main.java
@@ -0,0 +1,13 @@
+package no.ntnu.oskarlothe;
+
+/**
+ * A class representing the main starting point of the application.
+ * 
+ * @author Oskar Lothe
+ * @version 1.0-SNAPSHOT
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
