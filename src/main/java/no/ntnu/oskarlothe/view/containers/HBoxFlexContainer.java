17b098305a0dae52d4f351bad9e9ed59dade1788
Oskar <osklot12@gmail.com>
2023-08-02 01:31:04 +0200

    Fixed label centering issue for month display

--- src/main/java/no/ntnu/oskarlothe/view/containers/HBoxFlexContainer.java
+++ src/main/java/no/ntnu/oskarlothe/view/containers/HBoxFlexContainer.java
@@ -1,9 +1,11 @@
 package no.ntnu.oskarlothe.view.containers;
 
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
+import no.ntnu.oskarlothe.view.StyleApplier;
 
 /**
  * A class representing a JavaFX element responsible for holding children in a
@@ -12,7 +14,7 @@ import javafx.scene.layout.Priority;
  * @author Oskar Lothe
  * @version 1.0-SNAPSHOT
  */
-public class HBoxFlexContainer extends Pane {
+public class HBoxFlexContainer extends HBox {
     /**
      * Constructor for the HBoxFlexContainer class.
      * 
@@ -21,5 +23,8 @@ public class HBoxFlexContainer extends Pane {
     public HBoxFlexContainer(Node... children) {
         super(children);
         HBox.setHgrow(this, Priority.ALWAYS);
+
+        StyleApplier.addStyleClass(this, "hbox-flex-container");
+        StyleApplier.addStyleSheet(this, "HBoxFlexContainer.css");
     }
 }
---
+++ src/main/java/no/ntnu/oskarlothe/view/containers/VBoxFlexContainer.java
@@ -0,0 +1,27 @@
+package no.ntnu.oskarlothe.view.containers;
+
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import no.ntnu.oskarlothe.view.StyleApplier;
+
+/**
+ * A class representing a JavaFX component for a container flexing in the vertical direction.
+ * 
+ * @author Oskar Lothe
+ * @version 1.0-SNAPSHOT
+ */
+public class VBoxFlexContainer extends VBox {
+    /**
+     * Constructor for the VBoxFlexContainer class.
+     */
+    public VBoxFlexContainer(Node... children) {
+        super(children);
+        VBox.setVgrow(this, Priority.ALWAYS);
+        this.setAlignment(Pos.CENTER);
+
+        StyleApplier.addStyleClass(this, "vbox-flex-container");
+        StyleApplier.addStyleSheet(this, "VBoxFlexContainer.css");
+    }
+}
